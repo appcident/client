7726d78db3e41ba29abee1c7b3a015e5b325550f
anggraito <anggraito.laras@gmail.com>
2017-11-01 21:43:25 +0700

    app setting - clean code

--- accidentifier/App.js
+++ accidentifier/App.js
@@ -7,8 +7,8 @@ import Header from './src/components/Header'
 
 export default class App extends React.Component {
   render() {
-    const { region } = this.props;
-    console.log(region);
+    const { region } = this.props
+    console.log(region)
 
     return (
       <View style={styles.bodyWrap}>
@@ -25,8 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     top: 25,
     flexDirection: 'column',
-    // marginTop: 25,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
