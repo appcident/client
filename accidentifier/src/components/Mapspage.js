29d68288ccfadd0ec9162b37348dbeed173ee8a0
anggraito <anggraito.laras@gmail.com>
2017-11-02 10:56:11 +0700

    setting marker

--- accidentifier/src/components/Mapspage.js
+++ accidentifier/src/components/Mapspage.js
@@ -1,47 +1,86 @@
-import React from 'react';
+import React from 'react'
 import { StyleSheet, Text, 
-        View, Picker } from 'react-native';
-
-import MapView from 'react-native-maps'
+        View, Picker,
+        AppRegistry, Dimensions } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 // import Mapsi from './src/components/Maps'
 
+let { width, height } = Dimensions.get('window')
+const ASPECT_RATIO = width / height
+const LATITUDE = 59
+const LONGITUDE = 34
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
 export default class Maps extends React.Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super()
     this.state = {
-      state: '1 KM'
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
     }
   }
 
-  render() {
-    const { region } = this.props;
-    console.log(region);
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      },
+    (error) => console.log(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      }
+    );
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
 
+  render() {
+    // const { region } = this.props;
+    // console.log('regi on',region);
     return (
       <View style ={styles.container}>
         <MapView
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
+          provider={ PROVIDER_GOOGLE }
+          style={ styles.map }
+          showsUserLocation={ true }
+          showsCompass={true}
+          followsUserLocation={true}
+          region={ this.state.region }
+          onRegionChange={ region => this.setState({ region })}
+          onRegionChangeComplete={ region => this.setState({ region })}
         >
           <MapView.Marker 
-            coordinate={{
-              latitude: 37.78825,
-              longitude: -122.4324
-            }} 
-            title={"title"}
-            description={"description"}
+            coordinate={ this.state.region }
           />
         </MapView>
         <View style={styles.footerWrap}>
           <Picker
             style={{width: '40%'}}
-            selectedValue={this.state.language}
-            onValueChange={(lang) => this.setState({language: lang})}>
+            selectedValue={this.state.selectedRadius}
+            onValueChange={(radius) => this.setState({selectedRadius: radius})}>
             <Picker.Item label="1 KM" value="1" />
             <Picker.Item label="3 KM" value="2" />
             <Picker.Item label="5 KM" value="3" />
@@ -70,3 +109,5 @@ const styles = StyleSheet.create({
     alignItems: 'center' ,
   }
 });
+
+AppRegistry.registerComponent('Maps', () => Maps);
